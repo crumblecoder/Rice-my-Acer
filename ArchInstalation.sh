421d2234a1d2445a17de14ad03cfaf7b6ba69c25
crumblecoder <53544229+crumblecoder@users.noreply.github.com>
2020-04-18 03:51:27 +0200

    Update ArchInstalation.sh

--- ArchInstalation.sh
+++ ArchInstalation.sh
@@ -9,9 +9,17 @@ loadkeys es
 timedatectl set-ntp true
 mkfs.ext4 /dev/sda
 mount /dev/sdX1 /mnt
-pacstrap /mnt base linux linux-firmware networkmanager
+pacstrap /mnt base linux linux-firmware networkmanager grub
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
 hwclock --systohc
 sed -i  '/^#es_ES.*  /s/^#//'  /etc/locale.gen
+locale-gen
+echo "KEYMAP=es" > /etc/vconsole.conf
+echo "ACER-PC" >/etc/hostname
+echo "127.0.0.1	localhost" > /etc/hosts
+echo "::1		localhost"    >> /etc/hosts
+echo "127.0.1.1	ACER-PC"  >> /etc/hosts
+passwd
+
