b58839fab838a8adc49e505aee463f938b4cca0f
crumblecoder <53544229+crumblecoder@users.noreply.github.com>
2020-04-22 00:34:57 +0200

    Update afterfirstboot.sh

--- afterfirstboot.sh
+++ afterfirstboot.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -vx
 useradd -m -g wheel -s /bin/bash $(read -p "Escriba el nombre de usuario:" ;echo "$REPLY" ) -p $(openssl passwd -6 $(read -p "contraseña:";echo "$REPLY"))
+pacman -Sy
 git clone https://aur.archlinux.org/yay.git
 cd yay
+su $(ls /home/) <<EOF
 makepkg -si
-pacman -S alacritty  bspwm  sxhkd mesa-demos lemonbar xorg-xinit xorg
+exit
+EOF
+pacman -S alacritty  bspwm  sxhkd lemonbar xorg-xinit xorg
