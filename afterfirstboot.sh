013aa9dc79c4afcb6e01e5a95a7be5b44977a80f
crumblecoder <53544229+crumblecoder@users.noreply.github.com>
2020-04-22 01:17:21 +0200

    Update afterfirstboot.sh

--- afterfirstboot.sh
+++ afterfirstboot.sh
@@ -2,8 +2,8 @@
 useradd -m -g wheel -s /bin/bash $(read -p "Escriba el nombre de usuario:" ;echo "$REPLY" ) -p $(openssl passwd -6 $(read -p "contraseña:";echo "$REPLY"))
 sed -i '/# %wheel ALL=(ALL) ALL/s/^# //' /etc/sudoers
 pacman -Sy
-git clone https://aur.archlinux.org/yay.git
 su $(ls /home/) <<EOF
+git clone https://aur.archlinux.org/yay.git
 sudo cd yay
 makepkg -si
 exit
