b15946ec5bd4e1e38ce03ae9414c7bc160594619
crumblecoder <53544229+crumblecoder@users.noreply.github.com>
2020-04-22 00:51:39 +0200

    Update afterfirstboot.sh

--- afterfirstboot.sh
+++ afterfirstboot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -vx
 useradd -m -g wheel -s /bin/bash $(read -p "Escriba el nombre de usuario:" ;echo "$REPLY" ) -p $(openssl passwd -6 $(read -p "contraseña:";echo "$REPLY"))
+sed -i '/# %wheel ALL=(ALL) ALL/s/^# //' /etc/sudoers
 pacman -Sy
 git clone https://aur.archlinux.org/yay.git
 cd yay
