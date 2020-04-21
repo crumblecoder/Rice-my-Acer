317204f59f30631f4e198a7345f19db4fbb9c33a
crumblecoder <53544229+crumblecoder@users.noreply.github.com>
2020-04-22 01:26:12 +0200

    Update afterfirstboot.sh

--- afterfirstboot.sh
+++ afterfirstboot.sh
@@ -2,6 +2,7 @@
 useradd -m -g wheel -s /bin/bash $(read -p "Escriba el nombre de usuario:" ;echo "$REPLY" ) -p $(openssl passwd -6 $(read -p "contraseña:";echo "$REPLY"))
 sed -i '/# %wheel ALL=(ALL) ALL/s/^# //' /etc/sudoers
 pacman -Sy
+pacman -S alacritty  bspwm  sxhkd  xorg-xinit xorg go
 su $(ls /home/) <<EOF
 cd ~
 git clone https://aur.archlinux.org/yay.git
@@ -9,5 +10,4 @@ cd yay
 makepkg -si
 exit
 EOF
-pacman -S alacritty  bspwm  sxhkd  xorg-xinit xorg
 yay -S lemonbar
