75fa310f7c0a890005dc5dac4445723ca71383b5
crumblecoder <crumblecoder@gmail.com>
2020-05-01 08:38:59 +0200

    afterfirstboot.sh

--- afterfirstboot.sh
+++ afterfirstboot.sh
@@ -2,7 +2,7 @@
 useradd -m -g wheel -s /bin/bash $(read -p "Escriba el nombre de usuario:" ;echo "$REPLY" ) -p $(openssl passwd -6 $(read -p "contraseña:";echo "$REPLY"))
 sed -i '/# %wheel ALL=(ALL) ALL/s/^# //' /etc/sudoers
 pacman -Sy
-pacman -S alacritty  bspwm  sxhkd  xorg-xinit xorg go
+pacman -S alacritty  bspwm  sxhkd  xorg-xinit xorg go zsh zsh-completions
 cp ./00-keyboard.conf /etc/X11/xorg.conf.d/
 su $(ls /home/) << 'EOF'
 cd ~
